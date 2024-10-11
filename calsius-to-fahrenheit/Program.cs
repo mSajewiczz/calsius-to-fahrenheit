7baf208269c4b59eab010f4779815188961cab66
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2024-10-11 17:05:54 +0200

    Created converter

---
+++ calsius-to-fahrenheit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace calsius_to_fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
